15b9c7b7cc42bb90da46b6ad65f5ef63497df878
Ryan Cebulko <ryan@cebulko.com>
2016-12-11 18:08:54 -0500

    separated actual training from pre-training methods

--- classifier/AbstractClassifier.py
+++ classifier/AbstractClassifier.py
@@ -1,5 +1,7 @@
 from abc import abstractmethod
 
+
+
 class AbstractClassifier(object):
     """Arbitrary classifier implementation with helper methods"""
     def __init__(self, typ, classes, **params):
@@ -8,18 +10,22 @@ class AbstractClassifier(object):
         self.classes = classes # output classes ex: [0, 1, 2]
 
 
-    # train the model and store the features and outputs (possibly normalized or
-    # otherwise mutated)
+    # train the model
     @abstractmethod
-    def train(X, Y):
+    def _train(X, Y):
         pass
+
+    # perform any necessary normalization, store data, and train the model
+    def train(self, X, Y):
+        self.X, self.Y = X, Y
+        return self._train(X)
 
     # classify a set of test points; must accept an ndarray with shape (M, D)
     # where M is the number of test samples and D is the number of features; if
     # the classifier can only act on one point at a time, it must loop through
     # each sample
     @abstractmethod
-    def classify(test_X):
+    def classify(self, test_X):
         pass
 
     # list of binary {-1, 1} class labels for OVA on each output class
@@ -45,9 +51,9 @@ class AbstractClassifier(object):
 
 
     # counts the number of correctly classified test points
-    def correct(test_X, test_Y):
+    def correct(self, test_X, test_Y):
         return (self.classify(test_X) == test_Y).sum()
 
     # calculates the proportion of correctly classified test points (0-1)
-    def accuracy(test_X, test_Y):
+    def accuracy(self, test_X, test_Y):
         return self.correct(test_X, test_Y) / test_X.shape[0]
