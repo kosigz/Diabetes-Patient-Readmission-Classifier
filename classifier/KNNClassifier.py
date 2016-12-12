c23922c33cfb11f6800e1b48401651ece93861c7
Ryan Cebulko <ryan@cebulko.com>
2016-12-11 21:32:19 -0500

    minor comments and variable renames

--- classifier/KNNClassifier.py
+++ classifier/KNNClassifier.py
@@ -5,24 +5,24 @@ from . import AbstractZnormClassifier, test_classifier
 
 
 class KNNClassifier(AbstractZnormClassifier):
-    """Classifier which uses the K-nearest neighbor algorithms"""
+    """Classifier which uses the K-nearest neighbor algorithm"""
     def __init__(self, k, **kwargs):
         # keyword arguments are passed on to scikit-learn's KNN implementation
         # see http://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html#sklearn.neighbors.KNeighborsClassifier
-        # relevant kwargs:
-        #     weights (string): "uniform", "distance"
-        #     p (int): 1 (Manhattan distance), 2 (Euclidean distance)
-        #     n_jobs (int): 1 or more (used to parallelize neighbor search)
+        # relevant kwargs (* indicates default):
+        #     weights (string): "uniform"*, "distance"
+        #     p (int): 1 (Manhattan distance), 2* (Euclidean distance)
+        #     n_jobs (int): 1* or more (used to parallelize neighbor search)
         super(AbstractZnormClassifier, self).__init__("KNN", k=k, **kwargs)
-        self.knn = KNeighborsClassifier(n_neighbors=self.params["k"], **kwargs)
+        self._knn = KNeighborsClassifier(n_neighbors=self.params["k"], **kwargs)
 
     # train a KNN classifier on a provided dataset
     def _train(self, X, Y):
-        self.knn.fit(X, Y)
+        self._knn.fit(X, Y)
 
     # classify a set of test points
     def _classify(self, test_X):
-        return self.knn.predict(test_X)
+        return self._knn.predict(test_X)
 
 #for k in (3, 5, 10, 20):
 #    for w in ("uniform", "distance"):
