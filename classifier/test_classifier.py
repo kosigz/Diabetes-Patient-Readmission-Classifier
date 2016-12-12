b681ceda87ce80699ef0e3c7b49b712f0407ed78
Ryan Cebulko <ryan@cebulko.com>
2016-12-11 23:16:34 -0500

    cleaned up stale/dead test code

--- classifier/test_classifier.py
+++ classifier/test_classifier.py
@@ -1,26 +1,15 @@
 from sklearn import datasets
 from sklearn.cross_validation import train_test_split
 
+
 # import some test data
-#ds = datasets.load_iris()
-#X, Y = ds.data, ds.target
-X, Y = datasets.make_blobs(n_samples=1000, n_features=4, cluster_std=7, random_state=1)
+ds = datasets.load_iris()
+X, Y = ds.data, ds.target
+#X, Y = datasets.make_blobs(n_samples=1000, n_features=4, cluster_std=7, random_state=1)
 train_X, test_X, train_Y, test_Y = train_test_split(X, Y, random_state=1)
-
-def gen_clusters(*clusters):
-    import numpy as np
-
-    data, outputs = np.empty(0), np.empty(0)
-    for i, (mean, stdev, n) in enumerate(clusters):
-        data = np.append(data, np.random.normal(mean, stdev, n))
-        outputs = np.append(outputs, [i] * n)
-
-    return data[:,None], outputs
 
 
 def test_classifier_accuracy(classifier, n=1000):
-    m = n / 100
-
     classifier.train(train_X, train_Y)
     return classifier.accuracy(test_X, test_Y)
 
